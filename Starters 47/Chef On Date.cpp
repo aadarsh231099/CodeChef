6fa4001be6d5670f828784d965cbd58d44a1be50
Aadarsh <aadarshhanu23@gmail.com>
2022-07-18 15:59:09 +0530

    cc

---
+++ Starters 47/Chef On Date.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/submit/CHEFONDATE
+//Chef On Date
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int x,y;
+  cin>>x>>y;
+  if(x>=y)
+  cout<<"YES"<<endl;
+  else
+  cout<<"NO"<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
