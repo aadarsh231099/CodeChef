1effb4ee5e8fc3972046a81683d5a6138df8d9f6
Aadarsh <aadarshhanu23@gmail.com>
2022-07-18 16:00:58 +0530

    cc

---
+++ Starters 47/Chess Time.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/submit/CHESSTIME
+//Chess Time
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int n;
+  cin>>n;
+  cout<<(n*60)/20<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
