76b936503677e584bfcfa1d99cd49554429ae4c4
Aadarsh <aadarshhanu23@gmail.com>
2022-08-17 15:53:02 +0530

    cp

---
+++ Jun22 Long/Fitness.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/submit/FIT
+//Fitness
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x;
+    cin>>x;
+    cout<<5*2*x<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
