c952d4c8b4f2de23671bfc256d3e17287a4f897e
Aadarsh <aadarshhanu23@gmail.com>
2023-02-21 19:16:01 +0530

    cp

---
+++ Starters 44/Biryani.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/submit/BIRYANI
+//Biryani
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int x,y;
+  cin>>x>>y;
+  cout<<x*y<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
