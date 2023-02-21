be8a2c591293ed4899e6a71ad4d436553fb527f5
Aadarsh <aadarshhanu23@gmail.com>
2023-02-21 19:23:01 +0530

    cp

--- Starters 42/Age Limit.cpp
+++ Starters 42/Age Limit.cpp
@@ -1,11 +1,14 @@
-//https://www.codechef.com/problems/BIRYANI
-//Biryani
+//https://www.codechef.com/problems/AGELIMIT
+//Age Limit
 #include <bits/stdc++.h>
 using namespace std;
 void solve(){
-  int x,y;
-  cin>>x>>y;
-  cout<<x*y<<endl;
+  int x,y,a;
+  cin>>x>>y>>a;
+  if(a>=x && a<y)
+  cout<<"yes"<<endl;
+  else
+  cout<<"no"<<endl;
 }
 int main()
 {
