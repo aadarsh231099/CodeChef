08d8f6a0fcc7e56563754bf34fa6c4470f95d671
Aadarsh <aadarshhanu23@gmail.com>
2023-02-22 23:59:26 +0530

    cp

---
+++ Starters 29/Tyre problem.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/TYRE
+//Tyre Problem
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int m,n;
+  cin>>n>>m;
+  cout<<n*2+m*4<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
