8ee55b12d30c6ca8a3164af01113ec9c41f00524
Aadarsh <aadarshhanu23@gmail.com>
2023-03-02 01:24:53 +0530

    cp

---
+++ Starters 79/Ageing.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/AGEING
+//Ageing
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int x;
+  cin>>x;
+  cout<<x-10<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
