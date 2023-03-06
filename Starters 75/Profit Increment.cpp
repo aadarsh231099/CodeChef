8998c364f7fddd6fc177890e89e876278992d5e2
Aadarsh <aadarshhanu23@gmail.com>
2023-03-06 18:40:19 +0530

    cc

---
+++ Starters 75/Profit Increment.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/PROINC
+//Profit Increment
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int x,y;
+  cin>>x>>y;
+  cout<<(1.1*x)-(x-y)<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
