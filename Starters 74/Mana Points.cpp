ff2e0780827c39ad7a5b619d367a58a36b97cf07
Aadarsh <aadarshhanu23@gmail.com>
2023-03-08 10:52:24 +0530

    cc

---
+++ Starters 74/Mana Points.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/MANAPTS
+//Mana Points
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int x,y;
+  cin>>x>>y;
+  cout<<y/x<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
