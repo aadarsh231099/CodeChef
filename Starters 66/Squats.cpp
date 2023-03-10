00de8bcaed2c3de6d649129f63af0e67a991907c
Aadarsh <aadarshhanu23@gmail.com>
2023-03-11 01:32:31 +0530

    cc

---
+++ Starters 66/Squats.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/SQUATS
+//Squats
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x;
+    cin>>x;
+    cout<<15*x<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
