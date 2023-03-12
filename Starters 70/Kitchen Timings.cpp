6806eecd81a6bea0fd2b2319532dfa3f6a68ddb1
Aadarsh <aadarshhanu23@gmail.com>
2023-03-12 12:31:26 +0530

    cc

---
+++ Starters 70/Kitchen Timings.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/KITCHENTIME
+//Kitchen Timings
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x,y;
+    cin>>x>>y;
+    cout<<y-x<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
