245b03ebe7cb084e5174388f9072cf7548ff3d8a
Aadarsh <aadarshhanu23@gmail.com>
2023-03-17 02:06:10 +0530

    cc

---
+++ Starters 62/Man minus Min.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/MAXDIFFMIN
+//Max minus Min
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int a,b,c;
+  cin>>a>>b>>c;
+  cout<<c-a<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
