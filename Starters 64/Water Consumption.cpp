e8f7a8322cf240b9c8af7e22bf4087b5b0df19c1
Aadarsh <aadarshhanu23@gmail.com>
2023-03-18 07:13:23 +0530

    cc

---
+++ Starters 64/Water Consumption.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/WATERCONS
+//Water Consumption
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x;
+    cin>>x;
+    if(x>=2000)cout<<"Yes"<<endl;
+    else
+    cout<<"No"<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
