60b2eed912db5bb6f5995be65ecb8cc878516a62
Aadarsh <aadarshhanu23@gmail.com>
2023-03-20 12:04:10 +0530

    cc

---
+++ Starters 61/Reach on Time.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/TIMELY
+//Reach on Time
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x;
+    cin>>x;
+    if(x>=30)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
