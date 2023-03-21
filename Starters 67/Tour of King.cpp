5e43d0cae399dbbcdb90faf21a21ffd239795e92
Aadarsh <aadarshhanu23@gmail.com>
2023-03-21 12:36:06 +0530

    cc

---
+++ Starters 67/Tour of King.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/KNGTOR
+//Tour of King
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int n,m;
+  cin>>n>>m;
+  cout<<n*5+m*7<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
