487494161d3b23dde55151007b65e8dfddc9ae0d
Aadarsh <aadarshhanu23@gmail.com>
2023-03-28 14:08:05 +0530

    cc

---
+++ Starters 57/Alice and Marks.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/problems/MARKSTW
+//Alice and Marks
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x,y;
+    cin>>x>>y;
+    if(x>=2*y)cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t=1;
+  //cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
