6a79b4d5c2062fda0ba5d6d1fad23041ad743bf5
Aadarsh <aadarshhanu23@gmail.com>
2023-03-30 17:17:59 +0530

    cc

---
+++ Starters 83/Budget of Technex.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/START83D/problems/BUDTECH
+//Budget of Technex
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int r;
+    cin>>r;
+    cout<<((r*1000)/2)/5<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
