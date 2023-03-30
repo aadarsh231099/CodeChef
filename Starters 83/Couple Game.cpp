0aec917bca5f6d9c3e928415fb3da43119097eb8
Aadarsh <aadarshhanu23@gmail.com>
2023-03-30 17:17:11 +0530

    cc

---
+++ Starters 83/Couple Game.cpp
@@ -0,0 +1,21 @@
+//https://www.codechef.com/START83D/problems/COUGAME
+//Couple Game
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int g,b;
+    cin>>g>>b;
+    cout<<b-g<<endl;
+    
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
