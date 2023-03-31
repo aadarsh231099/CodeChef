72431c76a1ee73cb949a1194cd08055afab63c5f
Aadarsh <aadarshhanu23@gmail.com>
2023-03-31 13:26:44 +0530

    cc

---
+++ Starters 71/Instagram.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/INSTAGRAM
+//Instagram
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x,y;
+    cin>>x>>y;
+    if(x>10*y)cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
