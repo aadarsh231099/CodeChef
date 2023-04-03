22894fe57f24c3183becb38c5c09a409dec68a1b
Aadarsh <aadarshhanu23@gmail.com>
2023-04-04 00:29:26 +0530

    cc

---
+++ Starters 82/Candy Division.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/CANDIVIDE
+//Candy Division
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    if(n%3==0)cout<<"YES"<<endl;
+    else
+    cout<<"NO"<<endl;
+  
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
