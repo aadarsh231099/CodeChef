60497f113bda419c3d868c74c3018c1e6ca86ef2
Aadarsh <aadarshhanu23@gmail.com>
2023-04-06 02:07:43 +0530

    cc

---
+++ Starters 54/Number of Credits.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/CREDS
+//Number of Credits
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x,y,z;
+    cin>>x>>y>>z;
+    cout<<x*4+y*2<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
