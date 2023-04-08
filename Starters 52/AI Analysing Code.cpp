d979624ef9da2030e7aaf0422b485ea23bb42623
Aadarsh <aadarshhanu23@gmail.com>
2023-04-09 01:02:46 +0530

    cc

---
+++ Starters 52/AI Analysing Code.cpp
@@ -0,0 +1,22 @@
+//https://www.codechef.com/problems/AIANALYSE
+//AI Analysing Code
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int c;
+    cin>>c;
+    if(c<=1000) cout<<"Yes";
+    else
+    cout<<"no";
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t=1;
+  //cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
