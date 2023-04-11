474cca42b903c614f44f50da2f94afb216ea0671
Aadarsh <aadarshhanu23@gmail.com>
2023-04-11 11:51:16 +0530

    cc

---
+++ Starters 84/Chef and Donation.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/DNATION
+//Chef and Donation
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int x,y;
+    cin>>x>>y;
+    cout<<y-x<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
