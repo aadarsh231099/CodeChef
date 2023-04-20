6deff19758248b03f002ce6c44ebe7b43c23a9a3
Aadarsh <aadarshhanu23@gmail.com>
2023-04-20 12:01:12 +0530

    cc

---
+++ Starters 86/CodeChef Learn Problem Solving.cpp
@@ -0,0 +1,19 @@
+//https://www.codechef.com/problems/CCLEARN
+//CodeChef Learn Problem Solving
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+  int n;
+  cin>>n;
+  cout<<n*2<<endl;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t=1;
+  //cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
