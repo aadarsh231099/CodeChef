97651fbf6e425b53e2e84108141df3f603536c10
Aadarsh <aadarshhanu23@gmail.com>
2023-04-24 10:05:31 +0530

    cc

---
+++ Starters 57/Integers that sum to the same value.cpp
@@ -0,0 +1,20 @@
+//https://www.codechef.com/problems/SUMNEQ
+//Integers that sum to the same value
+
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int n;
+    cin>>n;
+    cout<<n-1;
+}
+int main()
+{
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int t=1;
+  //cin>>t;
+  while(t--)
+    solve();
+  return 0;
+}
